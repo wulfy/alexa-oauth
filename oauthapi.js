1aa4cf8c1631f036bdf1ac9f1c87592bf461b28b
ludovic <wulfy72@yahoo.fr>
2018-08-08 14:54:59 +0200

    gettoken works

--- oauthapi.js
+++ oauthapi.js
@@ -3,8 +3,16 @@
  */
 
 var mysql = require('mysql')
+var database = require('./database')
 
 var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'test_oauth'
+});
+
+var connectionDatabase = new database({
   host     : 'localhost',
   user     : 'root',
   password : 'root',
@@ -26,18 +34,17 @@ connection.connect(function(err) {
 
 module.exports.getAccessToken = function(bearerToken) {
   console.log("access token");
-  return connection.query('SELECT access_token, access_token_expires_on, client_id, refresh_token, refresh_token_expires_on, user_id FROM oauth_tokens WHERE access_token = ?', 
-    [bearerToken],
-    function (error, results, fields) {
-
-      if (error) throw error;
+  return connectionDatabase.query('SELECT access_token, access_token_expires_on, client_id, refresh_token, refresh_token_expires_on, user_id FROM oauth_tokens WHERE access_token = ?', 
+    [bearerToken])
+    .then(results => {
 
       var token = results[0];
-
+      console.log(token);
+      if(!token) return false;
       return {
         accessToken: token.access_token,
         client: {id: token.client_id},
-        expires: token.expires,
+        accessTokenExpiresAt: new Date(token.expires),
         user: {id: token.userId}, // could be any object
       };
     });
@@ -47,8 +54,27 @@ module.exports.getAccessToken = function(bearerToken) {
  * Get client.
  */
 
-module.exports.getClient = function *(clientId, clientSecret) {
+module.exports.getClient = function (clientId, clientSecret) {
   console.log("get client "+ clientId + " " + clientSecret);
+
+  return connectionDatabase.query('SELECT client_id, client_secret, redirect_uri FROM oauth_clients WHERE client_id = ? AND client_secret = ?', 
+    [clientId, clientSecret]).then( results => {
+        var oAuthClient = results[0];
+        console.log("return client");
+        if(!oAuthClient) return false;
+        console.log("data");
+        return {
+          "clientId": oAuthClient.client_id,
+          "clientSecret": oAuthClient.client_secret,
+          "grants": [
+                    "password",
+                    "authorization_code",
+                    "refresh_token"
+                ] // the list of OAuth2 grant types that should be allowed
+        };
+      }
+    );
+  /*
   return connection.query('SELECT client_id, client_secret, redirect_uri FROM oauth_clients WHERE client_id = ? AND client_secret = ?', 
     [clientId, clientSecret],
     function (error, results, fields) {
@@ -74,14 +100,72 @@ module.exports.getClient = function *(clientId, clientSecret) {
 
       console.log(returnData)
       return returnData;
-    });
+    });*/
 };
+
+
+module.exports.saveAuthorizationCode = function (code, client, user) {
+  console.log("saveAuthorizationCode");
+  console.log(client);
+  console.log(user);
+  return connectionDatabase.query('INSERT INTO  oauth_codes(expires, redirect_uri, client_id, user_id, code, scope) VALUES (?, ?, ?, ?, ?, ?)', 
+    [
+     code.expiresAt,
+     code.redirectUri,
+     client.id,
+     user.id,
+     code.authorizationCode,
+     code.scope
+     ]).then( results => {
+        
+        return {
+          ...code,
+          ...user,
+          ...client
+        };
+      }
+    );
+}; 
 
 /**
  * Get refresh token.
  */
 
-module.exports.getRefreshToken = function *(bearerToken) {
+module.exports.getAuthorizationCode = function (code) {
+  console.log("getAuthorizationCode");
+
+  return connectionDatabase.query('SELECT expires, redirect_uri, client_id, user_id, scope FROM oauth_codes WHERE code = ?', 
+    [code]).then( results => {
+        if(!results.length) return false;
+        let result = results[0];
+        console.log(result.user_id);
+        return {
+          code: code,
+          client: result.client_id,
+          expiresAt: new Date(result.expires),
+          redirectUri: result.redirect_uri,
+          user: result.user_id,
+          scope: result.scope,
+        };
+      }
+    );
+};
+
+module.exports.revokeAuthorizationCode = function (code) {
+  console.log("revokeAuthorizationCode " + code.code);
+  return connection.query("UPDATE oauth_codes SET expires='2018-08-08 00:00:00' WHERE code = ?", 
+    [code.code],
+    function (error, results ) {
+      if (error) throw error;
+      if(!results.length) return false;
+      return {
+        code: code,
+      }
+    });
+};
+
+
+module.exports.getRefreshToken = function (bearerToken) {
   console.log("getRefreshToken");
   return connection.query('SELECT access_token, access_token_expires_on, client_id, refresh_token, refresh_token_expires_on, user_id FROM oauth_tokens WHERE refresh_token = ?', 
     [bearerToken],
@@ -95,7 +179,7 @@ module.exports.getRefreshToken = function *(bearerToken) {
  * Get user.
  */
 
-module.exports.getUser = function *(username, password) {
+module.exports.getUser = function (username, password) {
   console.log("getUser");
   return connection.query('SELECT id FROM users WHERE username = ? AND password = ?', 
     [username, password],
@@ -109,18 +193,28 @@ module.exports.getUser = function *(username, password) {
  * Save token.
  */
 
-module.exports.saveAccessToken = function *(token, client, user) {
+module.exports.saveToken = function (token, client, user) {
   console.log("saveAccessToken");
-  return connection.query('INSERT INTO oauth_tokens(access_token, access_token_expires_on, client_id, refresh_token, refresh_token_expires_on, user_id) VALUES (?, ?, ?, ?)', 
+  console.log(user);
+  return connectionDatabase.query('INSERT INTO oauth_tokens(access_token, access_token_expires_on, client_id, refresh_token, refresh_token_expires_on, user_id) VALUES (?, ?, ?, ?, ?, ?)', 
     [
     token.accessToken,
-    token.accessTokenExpiresOn,
-    client.id,
+    token.accessTokenExpiresAt,
+    client.clientId,
     token.refreshToken,
-    token.refreshTokenExpiresOn,
-    user.id
-  ],function (error, results, fields) {
-      if (error) throw error;
-    return results.length ? results[0] : false; // TODO return object with client: {id: clientId} and user: {id: userId} defined
+    token.accessTokenExpiresAt,
+    user
+  ]).then( results => {
+      console.log("return save");
+
+      return {
+        accessToken: token.accessToken,
+        accessTokenExpiresAt: token.accessTokenExpiresAt,
+        refreshToken: token.refreshToken,
+        refreshTokenExpiresAt: token.accessTokenExpiresAt,
+        scope: token.scope,
+        client: {id: client.clientId},
+        user: {id: user}
+      }
   });
 };
